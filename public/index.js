9bdc188ca08ea889b6e813dc30b9417dc9b7fa82
bezz <b_e_z_z@outlook.com>
2020-10-03 19:20:24 +0100

    init

---
+++ public/index.js
@@ -0,0 +1,50 @@
+Vue.use(Vuetify);
+
+var vm = new Vue({
+    el: "#app",
+    comments: true,
+    data: function () {
+      return {
+            footerLoaded: false,
+            links: [
+                {
+                    label: 'twitter',
+                    link: 'https://twitter.com/b_e_z_z',
+                    img: './static/Twitter.png'
+                },
+                {
+                    label: 'itch.io',
+                    link: 'https://bezz.itch.io',
+                    img: './static/ItchIO.png'
+                },
+                {
+                    label: 'twitch',
+                    link: 'https://twitch.tv/b_e_z_z',
+                    img: './static/Twitch.png'
+                },
+                {
+                  label: 'artstation',
+                  link: 'https://www.artstation.com/bezz',
+                  img: './static/Artstation.png'
+                },
+                {
+                  label: 'instagram',
+                  link: 'https://www.instagram.com/b.e.z.z/',
+                  img: './static/Instagram.png'
+                }             
+                //{
+                //  label: 'youtube',
+                //  link: 'https://www.youtube.com/channel/UCEYnT8cCd5gz_IO8coSBbmw',
+                //  img: './static/YouTube.png'
+                //}
+            ]
+        };
+    },
+    methods: {
+        navigateToLink: function (link, e) {
+            var win = window.open(link.link, '_blank');
+            win.focus();
+            e.preventDefault();
+        },
+    }
+});
